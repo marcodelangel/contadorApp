cdf89f4a69e2003fca5b24f2210bb51fd6dab620
Marilyn García <marilyn_gm@outlook.com>
2017-05-26 20:37:11 -0500

    Suggested corrections

--- contadorApp/TableViewController.swift
+++ contadorApp/TableViewController.swift
@@ -8,107 +8,80 @@
 
 import UIKit
 
-class TableViewController: UITableViewController, NumberViewDelegate {
+class TableViewController: UITableViewController {
     
-    var numberArray = [Int]()
-    var stringArray = [String]()
-    var selectedIndex = -1
-
-
+    var numbers = [Int]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.navigationItem.leftBarButtonItem = self.editButtonItem
         
-        if let newStringArray = UserDefaults.standard.array(forKey: "strings") as? [String] {
-            stringArray = newStringArray
-        }
-        if let newNumberArray = UserDefaults.standard.array(forKey: "numbers") as? [Int] {
-            numberArray = newNumberArray
-        }
-    }
-
-    func didUpDateNumberArray(number:Int){
-
-        numberArray[selectedIndex] = number
-        stringArray[selectedIndex] = String(number)
-        print("Update NA \(numberArray)")
-        print("Update SA \(stringArray)")
-
-        
-        self.tableView.reloadData()
-        saveNumber()
-    }
-    
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        numbers = UserDefaults.standard.loadNumbers()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 
-        return stringArray.count
+        return numbers.count
     }
 
-    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-    
-        cell.textLabel?.text = stringArray[indexPath.row]
+        
+        cell.textLabel?.text = "\(numbers[indexPath.row])"
         
         return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selectedIndex = indexPath.row
-        performSegue(withIdentifier: "showNumber", sender: self)
+        performSegue(withIdentifier: "showNumber", sender: indexPath)
     }
-    
     
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         return true
     }
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            stringArray.remove(at: indexPath.row)
-            numberArray.remove(at: indexPath.row)
-            tableView.deleteRows(at: [indexPath], with: .fade)
-            saveNumber()
-        }
-    }
-    @IBAction func newNumber(_ sender: Any) {
         
-        switch numberArray.isEmpty {
-        case true:
-            let newNumber = 0
-            numberArray.append(newNumber)
-            stringArray.append(String(newNumber))
-            selectedIndex = 0
-        case false:
-            numberArray.append(0)
-            stringArray.append("0")
-            selectedIndex = numberArray.count-1
-        }
-
-        saveNumber()
-        self.tableView.reloadData()
-        performSegue(withIdentifier: "showNumber", sender: nil)
-    }
-    
-    func saveNumber(){
-        UserDefaults.standard.set(stringArray, forKey: "strings")
-        UserDefaults.standard.set(numberArray, forKey: "numbers")
-        UserDefaults.standard.synchronize()
+        guard editingStyle == .delete else { return }
+        
+        numbers.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .fade)
+        saveChanges()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         let vc = segue.destination as! ViewController
         vc.numberDelegate = self
-        vc.number = numberArray[selectedIndex]
-        print ("Index \(selectedIndex)")
-        print("NA \(numberArray)")
-        print("SA \(stringArray)")
+        
+        if let indexPath = sender, let indexPathForValue = indexPath as? IndexPath {
+            vc.number = (value: numbers[indexPathForValue.row], indexPath: indexPathForValue)
+        } else {
+            vc.number = (value: 0, indexPath: nil)
+        }
+    }
+    
+    fileprivate func saveChanges(){
+        UserDefaults.standard.saveNumbers(numbers)
+        self.tableView.reloadData()
+    }
+}
+
+extension TableViewController {
+    @IBAction func newNumber(_ sender: Any) {
+        performSegue(withIdentifier: "showNumber", sender: nil)
+    }
+}
+
+extension TableViewController : NumberViewDelegate {
+    
+    func update(number: Int, atIndexPath: IndexPath) {
+        numbers[atIndexPath.row] = number
+        saveChanges()
+    }
+    
+    func save(number: Int) {
+        numbers.append(number)
+        saveChanges()
     }
 }
--- contadorApp/ViewController.swift
+++ contadorApp/ViewController.swift
@@ -9,32 +9,60 @@
 import UIKit
 
 protocol NumberViewDelegate {
-    func didUpDateNumberArray(number:Int)
+    func save(number: Int)
+    func update(number: Int, atIndexPath: IndexPath)
 }
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
     
-    var numberDelegate : NumberViewDelegate?
+    var number: (value: Int, indexPath: IndexPath?) = (value: 0, indexPath: nil)
+    
+    var numberDelegate : NumberViewDelegate!
 
-    @IBOutlet weak var textField: UITextField!
-    var number : Int = 0
+    @IBOutlet weak var textField: UITextField! {
+        didSet {
+            textField.delegate = self
+            textField.text = "\(number.value)"
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        textField.delegate = self
-        textField.text = String(number)
+        
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
-
     }
     
-    /*override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
+    fileprivate func add(otherNumber: Int) {
+        number.value = number.value + otherNumber
+        textField.text = "\(number.value)"
+    }
+}
+
+extension ViewController {
+    @IBAction func add() {
+        add(otherNumber: 1)
+    }
+    
+    @IBAction func decrease() {
+        add(otherNumber: -1)
+    }
+    
+    @IBAction func reset() {
+        add(otherNumber: -1 * number.value)
+    }
+    
+    @IBAction func saveNumber() {
+        textField.resignFirstResponder()
         
-        if numberDelegate != nil{
-            numberDelegate?.didUpDateNumberArray(number: number)
+        if let indexPath = number.indexPath {
+            numberDelegate.update(number: number.value, atIndexPath: indexPath)
+        } else {
+            numberDelegate.save(number: number.value)
         }
-    }*/
+    }
+}
+
+extension ViewController : UITextFieldDelegate {
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
@@ -42,48 +70,21 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
-        
-        textField.text = ""
-        
-        guard CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: textField.text!)) else {
-            
-            textField.text = "Anota un número"
-            
-            return
+        guard let text = textField.text,
+            CharacterSet.decimalDigits.isSuperset(of: CharacterSet(charactersIn: text)) else {
+                textField.text = "Anota un número"
+                return
         }
     }
     
     func textFieldDidEndEditing(_ textField: UITextField, reason: UITextFieldDidEndEditingReason) {
-        if textField.text != "" {
-            number = Int(textField.text!)!
-        }
-    }
-    
-    @IBAction func add(_ sender: Any) {
-    number = number + 1
-    textField.text = "\(number)"
-    print(number)
-    }
-    
-    @IBAction func decrease(_ sender: Any) {
-    number = number - 1
-    textField.text = "\(number)"
-    print(number)
-    }
-   
-    @IBAction func reset(_ sender: Any) {
-    number = 0
-    textField.text = ""
-    print(number)
-    }
-    
-    @IBAction func saveNumber(_ sender: Any) {
         
-        textField.resignFirstResponder()
-    
-        if numberDelegate != nil{
-            numberDelegate?.didUpDateNumberArray(number: number)
+        guard let text = textField.text, let integerOnlyText = Int(text) else {
+            return
         }
+        
+        number.value = integerOnlyText
     }
 }
+
 
